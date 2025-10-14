631ca591cd56bd6b9980070defc251bd67def145
Ashutosh Rath <ashutoshrath1612@gmail.com>
2025-10-15 00:33:16 +0530

    Minor Responsive changes

--- client/src/app/(pages)/about/RequestDemo.jsx
+++ client/src/app/(pages)/about/RequestDemo.jsx
@@ -1,4 +1,8 @@
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
+
 export default function RequestDemo() {
+  const router = useRouter();
   return (
     <section className="text-center py-16 bg-white text-black">
       <h1 className="text-2xl md:text-3xl font-bold mb-4 text-black">
@@ -8,9 +12,11 @@ export default function RequestDemo() {
         Join the growing number of schools that are enhancing their educational
         experience with Edusphere.
       </p>
-      <button className="bg-blue-600 text-white px-6 py-3 rounded-md font-medium hover:bg-blue-700 transition">
+      <Button className="bg-blue-600 text-white px-6 py-3 rounded-md font-medium hover:bg-blue-700 transition" 
+              onClick={()=>router.push("/demo")}
+              variant="contained">
         Request a Free Demo
-      </button>
+      </Button>
     </section>
   );
 }
