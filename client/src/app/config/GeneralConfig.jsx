9fd4cb3fc0fd1c7c62b2c26798b7ca6131885204
Ashutosh Rath <ashutoshrath1612@gmail.com>
2025-11-01 00:41:18 +0530

    Logout fix

--- client/src/app/config/GeneralConfig.jsx
+++ client/src/app/config/GeneralConfig.jsx
@@ -57,7 +57,7 @@ export const userMenuItems = [
     label: "Logout",
     icon: <Logout fontSize="small" />,
     action: "logout",
-    actionValue: "/auth/login",
+    actionValue: "/auth/logout",
   },
 ];
 
